23d5322f3df27be190df5b6f6884048a5918ba6b
drbean <drbean@b2c4821f-8ecf-e411-a443-005056b105e6>
2013-05-11 03:39:57 +0000

    TabBarDecoration,SimpleDecoration

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -10,15 +10,21 @@ import XMonad.Config.Kde
 import XMonad.Config.Xfce
 
 import XMonad.Layout.Tabbed
+import XMonad.Layout.TabBarDecoration
+import XMonad.Layout.SimpleDecoration
+import XMonad.Layout.Accordion
+import XMonad.Util.Themes
 
-myLayout = simpleTabbed ||| Full
+-- myLayout = Tiled ||| tabbed shrinkText defaultTheme ||| Accordion ||| tabbed shrinkText defaultTheme ||| simpleTabbed ||| tabbed ||| Full ||| addTabs ||| tabbed shrinkText (theme donaldTheme) ||| (Tall 1 (3/100) (1/2))
+myLayout = simpleDeco shrinkText defaultTheme (layoutHook desktopConfig)
 
 main = do
      session <- getEnv "DESKTOP_SESSION"
-     -- xmonad  $ maybe desktopConfig desktop session
+     -- xmonad  defaultConfig
      xmonad  ( maybe desktopConfig desktop session )
 		{ modMask = mod4Mask
 		, terminal="gnome-terminal --hide-menubar"
+		-- , terminal = "/usr/bin/urxvt"
 		, layoutHook = myLayout
 		}
 

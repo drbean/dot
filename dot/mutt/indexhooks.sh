87e8f39d5cedf88fbdcc546af1936d7b51d2df20
drbean <drbean@b2c4821f-8ecf-e411-a443-005056b105e6>
2014-04-14 14:27:30 +0000

    TODO:unknown_command

--- dot/mutt/indexhooks.sh
+++ dot/mutt/indexhooks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd $HOME/Mail;
+cd "$HOME/Mail";
 
 # printf "folder-hook . 'macro index h <change-folder>?<tab><jump><enter>0<enter>'\n";
 # printf "folder-hook '!' 'macro index h <change-folder>?<tab><jump><enter>1<enter>'\n";

96aff539816239c686a65f499329640b9005f4c1
drbean <drbean@b2c4821f-8ecf-e411-a443-005056b105e6>
2014-08-14 04:23:33 +0000

    drbean:gsettings set org.mate.background picture-filename /home/drbean/Pictures/$pic

---
+++ dot/background/picture_uri.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use IO::All;
+use List::Util "shuffle";
+
+my $unseen = io('/home/drbean/unseen_pics.txt');
+my $seen = io('/home/drbean/seen_pics.txt');
+
+my @raw_unseen = $unseen->slurp;
+my @seen = $seen->slurp;
+
+my @unseen = shuffle @raw_unseen;
+my $pic = $unseen[0];
+
+$unseen->print(@unseen[1..$#unseen]); 
+$seen->print(@seen, $pic);
+
+chomp $pic;
+
+system("gsettings set org.mate.background picture-filename /home/drbean/Pictures/$pic");

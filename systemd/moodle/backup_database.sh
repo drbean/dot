f9f4e05f5bf94df5e0566e67ffe5a84c6260b805
drbean <drbean@freeshell.org>
2017-08-27 21:30:00 +0800

    s/pg_dump/Moosh_sql-dump/,s/apache/drbean/,PGHOST,PGUSER.PGPASSWORD,PGPORT:not_necessary

--- systemd/moodle/backup_database.sh
+++ systemd/moodle/backup_database.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-PGHOST='127.0.0.1' PGUSER='apache' PGPASSWORD=$1 PGPORT=5433 pg_dump $2 > ~drbean/dot/postgres/moodle$SEMESTER.sql
+cd /var/www/cgi-bin/moodle && sudo -u drbean /usr/local/bin/Moosh sql-dump > ~drbean/dot/postgres/moodle$SEMESTER.sql
 FTP_PASSWORD=$3 PASSPHRASE=$4 duplicity ~drbean/dot/postgres/moodle$SEMESTER.sql ftp://greg@web.nuu.edu.tw/duplicity/moodle$SEMESTER.sql
 FTP_PASSWORD=$3 PASSPHRASE=$4 duplicity /var/lib/moodle ftp://greg@web.nuu.edu.tw/duplicity/sac_moodle_var_lib_fedora25

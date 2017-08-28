eaa29a4b27636c04d00831c91ae79a2dcad82ce7
drbean <drbean@freeshell.org>
2017-08-28 15:19:50 +0800

    PGPASSWORD,DATABASE.out.positional_parameters_s/3,4/1,2/

--- systemd/moodle/backup_database.sh
+++ systemd/moodle/backup_database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/cgi-bin/moodle && sudo -u drbean /usr/local/bin/Moosh sql-dump > ~drbean/dot/postgres/moodle$SEMESTER.sql
-FTP_PASSWORD=$3 PASSPHRASE=$4 duplicity ~drbean/dot/postgres/moodle$SEMESTER.sql ftp://greg@web.nuu.edu.tw/duplicity/moodle$SEMESTER.sql
-FTP_PASSWORD=$3 PASSPHRASE=$4 duplicity /var/lib/moodle ftp://greg@web.nuu.edu.tw/duplicity/sac_moodle_var_lib_fedora25
+FTP_PASSWORD=$1 PASSPHRASE=$2 duplicity ~drbean/dot/postgres/moodle$SEMESTER.sql ftp://greg@web.nuu.edu.tw/duplicity/moodle$SEMESTER.sql
+FTP_PASSWORD=$1 PASSPHRASE=$2 duplicity /var/lib/moodle ftp://greg@web.nuu.edu.tw/duplicity/sac_moodle_var_lib_fedora25

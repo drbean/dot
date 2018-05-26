424c3a207b79f5efefc17befb7a5723b2546431a
drbean <drbean@freeshell.org>
2018-05-26 10:51:28 +0800

    Moosh -n

--- systemd/moodle/backup_database.sh
+++ systemd/moodle/backup_database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /var/www/cgi-bin/moodle && sudo -u drbean /usr/local/bin/Moosh sql-dump > ~drbean/dot/postgres/moodle$SEMESTER.sql
+cd /var/www/cgi-bin/moodle && sudo -u drbean /usr/local/bin/Moosh -n sql-dump > ~drbean/dot/postgres/moodle$SEMESTER.sql
 FTP_PASSWORD=$1 PASSPHRASE=$2 duplicity ~drbean/dot/postgres/moodle$SEMESTER.sql ftp://greg@web.nuu.edu.tw/duplicity/moodle$SEMESTER.sql
 FTP_PASSWORD=$1 PASSPHRASE=$2 duplicity /var/lib/moodle ftp://greg@web.nuu.edu.tw/duplicity/sac_moodle_var_lib_fedora25

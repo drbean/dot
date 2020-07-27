1ad2ef3de2b5287629693c1ca77ce6d85c48e827
drbean <drbean@freeshell.org>
2020-07-27 12:16:47 +0800

    no_pg_dump,no-encryption,s/web.nuu/sdf/

--- systemd/moodle/backup_database.sh
+++ systemd/moodle/backup_database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo -u postgres pg_dump -p 5433 mood$SEMESTER > ~drbean/dot/postgres/moodle$SEMESTER.sql
-FTP_PASSWORD=$1 PASSPHRASE=$2 duplicity ~drbean/dot/postgres/moodle$SEMESTER.sql ftp://greg@web.nuu.edu.tw/duplicity/moodle$SEMESTER.sql
-FTP_PASSWORD=$1 PASSPHRASE=$2 duplicity /var/lib/moodle ftp://greg@web.nuu.edu.tw/duplicity/sac_moodle_var_lib_fedora31
+# sudo -u postgres pg_dump -p 5433 mood$SEMESTER > ~drbean/dot/postgres/moodle$SEMESTER.sql
+FTP_PASSWORD=$1 duplicity --no-encryption ~drbean/dot/postgres/moodle$SEMESTER.sql ftp://drbean@sdf.org/duplicity/moodle$SEMESTER.sql
+FTP_PASSWORD=$1 duplicity --no-encryption /var/lib/moodle ftp://drbean@sdf.org/duplicity/sac_moodle_var_lib_fedora31

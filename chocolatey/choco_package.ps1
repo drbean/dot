fe98973a4578e672f98e9296f26ebbf6d5bfaebd
drbean <drbean@freeshell.org>
2020-09-01 12:29:51 +0800

    ntu_cygwin_repo.sourceware_out?

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,4 +1,4 @@
-ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/sourceware/cygwin" }
+ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
 $ps = "subversion", "openssh", "lftp", "vim", "screen", "mutt", "znc", "exim" 
 ForEach ($p in $ps) { choco install $p --source=cygwin }
 ForEach ($p in "qutebrowser") { choco install $p.install }

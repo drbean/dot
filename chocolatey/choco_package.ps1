e15b207cfc8ed9af4e27a2e5694aae379caf2291
drbean <drbean@freeshell.org>
2020-09-01 11:35:41 +0800

    powershell_foreach_loop

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,3 +1,4 @@
-for p in cygwin ; do choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/sourceware/cygwin"
-for p in subversion openssh lftp vim screen mutt znc exim ; do choco install $p --source=cygwin; done
-for p in qutebrowser ; do choco install $p.install ; done
+ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/sourceware/cygwin" }
+$ps = "subversion", "openssh", "lftp", "vim", "screen", "mutt", "znc", "exim" 
+ForEach ($p in $ps) { choco install $p --source=cygwin }
+ForEach ($p in "qutebrowser") { choco install $p.install }

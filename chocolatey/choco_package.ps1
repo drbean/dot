a7e1d1a7b8e491e4b5a5581be8e4e596df4fd092
drbean <drbean@freeshell.org>
2020-09-01 12:51:28 +0800

    qutebrowser.no_powershell_script_in_cygwin_shell?

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,4 +1,5 @@
 ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
 $ps = "subversion", "openssh", "lftp", "vim", "screen", "mutt", "znc", "exim" 
 ForEach ($p in $ps) { choco install $p --source=cygwin }
-ForEach ($p in "qutebrowser") { choco install $p.install }
+$qs = "qutebrowser", "qutebrowser.install"
+ForEach ($q in $qs) { choco install $q }

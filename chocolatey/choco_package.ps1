a5f5dde566d2253e164850f5965099f6b88a785d
drbean <drbean@freeshell.org>
2020-09-01 16:28:21 +0800

    weechat,znc,lynx

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,5 +1,5 @@
 ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
-$ps = "subversion", "openssh", "lftp", "vim", "screen", "mutt", "znc", "exim" 
+$ps = "subversion", "openssh", "lftp", "vim", "screen", "mutt", "znc", "exim", "weechat", "znc", "lynx"
 ForEach ($p in $ps) { choco install $p --source=cygwin }
 $qs = "qutebrowser", "qutebrowser.install"
 ForEach ($q in $qs) { choco install $q }

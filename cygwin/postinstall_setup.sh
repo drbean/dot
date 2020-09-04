b59e6f0b692f5088f16aa9aed73af0305db89330
drbean <drbean@freeshell.org>
2020-09-04 10:52:08 +0800

    svn_s/cat/up/_alternatives.sh

--- cygwin/postinstall_setup.sh
+++ cygwin/postinstall_setup.sh
@@ -3,5 +3,5 @@ for p in dot edit ttb ; do svn co http://github.com/drbean/$p/trunk $p --depth=e
 for u in bash screen chocolatey cygwin browser irc; do svn up dot/$u ; done
 # svn up edit/joyce
 for ext in rc _profile ; do cp dot/bash/bash$ext ~/.bash$ext ; done
-cd ~/dot && svn cat ^/trunk/alternatives.sh > alternatives.sh && . alternatives.sh && cd -
+cd ~/dot && svn up alternatives.sh && . alternatives.sh && cd -
 cp ~/dot/browser/lynx{-site.cfg,.lss} /etc/

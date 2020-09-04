fa4df870509df14219dc72aab3b00ab1ef4c4eb8
drbean <drbean@freeshell.org>
2020-09-04 10:48:05 +0800

    enable_feature:allowGlobalConfirmation

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,3 +1,4 @@
+choco feature enable -n allowGlobalConfirmation
 ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
 $ps = "subversion", "openssh", "lftp", "vim", "screen", "mutt", "znc", "exim", "weechat", "znc", "lynx"
 ForEach ($p in $ps) { choco install $p --source=cygwin }

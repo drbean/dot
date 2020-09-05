8fae89f4f60dba49cbbf7dab70428408859989f9
drbean <drbean@freeshell.org>
2020-09-05 17:29:14 +0800

    co_curriculum.up_pages,co_pandoc-templates

--- cygwin/cyg_setup.sh
+++ cygwin/cyg_setup.sh
@@ -1,5 +1,5 @@
 cd
-for p in dot edit ttb ; do svn co http://github.com/drbean/$p/trunk $p --depth=empty ; done
+for p in dot edit ttb curriculum; do svn co http://github.com/drbean/$p/trunk $p --depth=empty ; done
 for u in bash screen chocolatey cygwin browser irc; do svn up dot/$u ; done
 # svn up edit/joyce
 for ext in rc _profile ; do cp dot/bash/bash$ext ~/.bash$ext ; done
@@ -7,3 +7,5 @@ cd ~/dot && svn up alternatives.sh && . alternatives.sh && cd -
 cp ~/dot/screen/screen.rc ~/.screenrc
 cp ~/dot/browser/lynx{-site.cfg,.lss} /etc/
 for i in znc weechat ; do svn co http://github.com/drbean/dot/trunk/irc/$i ~/.$i ; done
+for p in pages ; do svn up curriculum/$p ; done
+for p in pandoc-templates ; do svn co http://github.com/drbean/$p/trunk ~/curriculum/pages/$p

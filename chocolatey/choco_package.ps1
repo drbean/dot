629b121646109593f6d61c8a67471ee043c6b5dc
drbean <drbean@freeshell.org>
2020-09-05 12:19:42 +0800

    chocolatey.pandoc_package

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -2,5 +2,5 @@ choco feature enable -n allowGlobalConfirmation
 ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
 $ps = "subversion", "openssh", "lftp", "vim", "screen", "mutt", "znc", "exim", "weechat", "znc", "lynx"
 ForEach ($p in $ps) { choco install $p --source=cygwin }
-$qs = "qutebrowser", "qutebrowser.install"
+$qs = "qutebrowser pandoc", "qutebrowser.install"
 ForEach ($q in $qs) { choco install $q }

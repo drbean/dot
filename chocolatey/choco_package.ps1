72d96dda06268816fb9a1a136c8f6b1cb521988e
drbean <drbean@freeshell.org>
2020-09-07 10:34:41 +0800

    cygwin_znc,weechat,after_svn,ssh

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,6 +1,6 @@
 choco feature enable -n allowGlobalConfirmation
 ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
-$ps = "subversion", "openssh", "lftp", "vim", "screen", "mutt", "znc", "exim", "weechat", "znc", "lynx"
+$ps = "subversion", "openssh", "znc", "weechat", "lftp", "vim", "screen", "mutt", "exim", "lynx"
 ForEach ($p in $ps) { choco install $p --source=cygwin }
 $qs = "qutebrowser pandoc", "qutebrowser.install"
 ForEach ($q in $qs) { choco install $q }

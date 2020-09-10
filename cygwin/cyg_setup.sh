9221f212c101bef416f7fb8d597464775701d40e
drbean <drbean@freeshell.org>
2020-09-10 09:56:04 +0800

    mo_dot/irc_update,run_znc

--- cygwin/cyg_setup.sh
+++ cygwin/cyg_setup.sh
@@ -1,6 +1,6 @@
 cd
 for p in dot edit ttb curriculum; do svn co http://github.com/drbean/$p/trunk $p --depth=empty ; done
-for u in bash screen vim chocolatey cygwin browser irc; do svn up dot/$u ; done
+for u in bash screen vim chocolatey cygwin browser ; do svn up dot/$u ; done
 # svn up edit/joyce
 for ext in rc _profile ; do cp dot/bash/bash$ext ~/.bash$ext ; done
 cd ~/dot && svn up alternatives.sh && . alternatives.sh && cd -
@@ -10,3 +10,4 @@ cp ~/dot/browser/lynx{-site.cfg,.lss} /etc/
 for i in znc weechat ; do svn co http://github.com/drbean/dot/trunk/irc/$i ~/.$i ; done
 for p in pages ; do svn up curriculum/$p ; done
 for p in pandoc-templates ; do svn co http://github.com/drbean/$p/trunk ~/curriculum/pages/$p ; done
+znc

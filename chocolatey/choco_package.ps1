fc97c2f1d8eb11a0c16b9e59a843b57323ed29fd
drbean <drbean@freeshell.org>
2020-09-12 16:38:50 +0800

    s/choco/cygwinsetup/,for_ftp.ntu.edu.tw_speed

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,6 +1,7 @@
 choco feature enable -n allowGlobalConfirmation
 ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
 $ps = "subversion", "openssh", "znc", "weechat", "lftp", "vim", "screen", "mutt", "exim", "lynx"
-ForEach ($p in $ps) { choco install $p --source=cygwin }
+# ForEach ($p in $ps) { choco install $p --source=cygwin }
+ForEach ($p in $ps) { c:/tools/cygwin/cygwinsetup.exe --quiet-mode --no-desktop --no-startmenu --root c:/tools/cygwin --local-package-dir c:/tools/cygwin/packages --site http://ftp.ntu.edu.tw/cygwin/ --packages $p }
 $qs = "qutebrowser", "pandoc"
 ForEach ($q in $qs) { choco install $q }

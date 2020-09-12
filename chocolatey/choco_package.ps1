5a332a9c7d6b6dfea35788421366aacbfafcadf4
drbean <drbean@freeshell.org>
2020-09-12 21:56:40 +0800

    cygwinsetup:not_working

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,7 +1,8 @@
 choco feature enable -n allowGlobalConfirmation
 ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
 $ps = "subversion", "openssh", "znc", "weechat", "lftp", "vim", "screen", "mutt", "exim", "lynx"
-# ForEach ($p in $ps) { choco install $p --source=cygwin }
-ForEach ($p in $ps) { c:/tools/cygwin/cygwinsetup.exe --quiet-mode --no-desktop --no-startmenu --root c:/tools/cygwin --local-package-dir c:/tools/cygwin/packages --site http://ftp.ntu.edu.tw/cygwin/ --packages $p }
+ForEach ($p in $ps) { choco install $p --source=cygwin }
+# $pstring = "subversion openssh znc weechat lftp vim screen mutt exim lynx weechat-perl weechat-python"
+# c:/tools/cygwin/cygwinsetup.exe --quiet-mode --no-desktop --no-startmenu --root c:/tools/cygwin --local-package-dir c:/tools/cygwin/packages --site http://ftp.ntu.edu.tw/cygwin/ --packages $pstring
 $qs = "qutebrowser", "pandoc"
 ForEach ($q in $qs) { choco install $q }

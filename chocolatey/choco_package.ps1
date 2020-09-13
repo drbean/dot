8342a5b6052216b764ffe341a787b62841348e66
drbean <drbean@freeshell.org>
2020-09-13 12:50:21 +0800

    weechat-perl,weechat-python

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,6 +1,6 @@
 choco feature enable -n allowGlobalConfirmation
 ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
-$ps = "subversion", "openssh", "znc", "weechat", "lftp", "vim", "screen", "mutt", "exim", "lynx"
+$ps = "subversion", "openssh", "znc", "weechat", "lftp", "vim", "screen", "mutt", "exim", "lynx", "weechat-perl", "weechat-python"
 ForEach ($p in $ps) { choco install $p --source=cygwin }
 # $pstring = "subversion openssh znc weechat lftp vim screen mutt exim lynx weechat-perl weechat-python"
 # c:/tools/cygwin/cygwinsetup.exe --quiet-mode --no-desktop --no-startmenu --root c:/tools/cygwin --local-package-dir c:/tools/cygwin/packages --site http://ftp.ntu.edu.tw/cygwin/ --packages $pstring

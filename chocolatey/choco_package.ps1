7f930047613dca9eace1e0f83b6c5f32c243d3e8
drbean <drbean@freeshell.org>
2020-09-22 12:14:20 +0800

    comment_out_cygwin_packages,leaving_cygwin,qutebrowser,pandoc

--- chocolatey/choco_package.ps1
+++ chocolatey/choco_package.ps1
@@ -1,7 +1,7 @@
 choco feature enable -n allowGlobalConfirmation
 ForEach ($p in "cygwin") { choco install $p --params "/DesktopIcon /Site:http://ftp.ntu.edu.tw/cygwin" }
-$ps = "subversion", "openssh", "znc", "screen", "weechat", "lftp", "vim", "mutt", "exim", "lynx", "weechat-perl", "weechat-python", "perl", "make"
-ForEach ($p in $ps) { choco install $p --source=cygwin }
+# $ps = "subversion", "openssh", "znc", "screen", "weechat", "lftp", "vim", "mutt", "exim", "lynx", "weechat-perl", "weechat-python", "perl", "make"
+# ForEach ($p in $ps) { choco install $p --source=cygwin }
 # $pstring = ""
 # ForEach ($p in $ps) { $pstring = $pstring + " " + $p }
 # c:/tools/cygwin/cygwinsetup.exe --quiet-mode --no-desktop --no-startmenu --root c:/tools/cygwin --local-package-dir c:/tools/cygwin/packages --site http://ftp.ntu.edu.tw/cygwin/ --packages $pstring

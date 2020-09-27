ed014a7b2e6aea3928e29cd27ee696cf7638ca9e
drbean <drbean@freeshell.org>
2020-09-27 13:18:21 +0800

    munge_pg_hba.conf.s/trust/password/

--- cygwin/pg_setup.sh
+++ cygwin/pg_setup.sh
@@ -5,6 +5,10 @@ sed -iBAK -r -e \"s/\^INITDB_ARGS=.*$/INITDB_ARGS=\\\"-E UTF8 --locale=en_US.UTF
 cygserver-config
 cygrunsrv --start cygserver
 for a in initdb install start ; do  /etc/rc.d/init.d/postgresql \$a ; done
+sed -iBAK -r -e \
+	"/host    all *all *127.0.0.1\/32 */s/trust/password/" -e \
+	"/host    all *all *::1\/128 */s/trust/password/"\
+	/usr/share/postgresql/data/pg_hba.conf
 psql -U SYSTEM postgres
 -- \i ~/dot/postgres/populate.sql
 

fc0767228350f676d28361ebe9dfb96b2cd00096
drbean <drbean@freeshell.org>
2020-09-27 21:50:42 +0800

    sed,for_loop:unescaping_vars

--- cygwin/pg_setup.sh
+++ cygwin/pg_setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-for p in postgresql libpq5 libpq-devel libpgtypes3 postgresql-client postgresql-devel ; do cyg_pack -p \$p ; done
-sed -iBAK -r -e \"s/\^INITDB_ARGS=.*$/INITDB_ARGS=\\\"-E UTF8 --locale=en_US.UTF-8 -D \\\$PGDATA\\\"/\" /etc/rc.d/init.d/postgresql
+for p in postgresql libpq5 libpq-devel libpgtypes3 postgresql-client postgresql-devel ; do cyg_pack -p $p ; done
+sed -iBAK -r -e "s/^INITDB_ARGS=.*$/INITDB_ARGS=\"-E UTF8 --locale=en_US.UTF-8 -D \$PGDATA\"/" /etc/rc.d/init.d/postgresql
 cygserver-config
 cygrunsrv --start cygserver
-for a in initdb install start ; do  /etc/rc.d/init.d/postgresql \$a ; done
+for a in initdb install start ; do  /etc/rc.d/init.d/postgresql $a ; done
 sed -iBAK -r -e \
 	"/host    all *all *127.0.0.1\/32 */s/trust/password/" -e \
 	"/host    all *all *::1\/128 */s/trust/password/"\

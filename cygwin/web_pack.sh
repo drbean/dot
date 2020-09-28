005d0f088a732689bdc382e2576068afc492c46f
drbean <drbean@freeshell.org>
2020-09-28 11:41:30 +0800

    cygwin_httpd,php_packages:cyg_pack.s/do_loop/comma-separated_list/

--- cygwin/web_pack.sh
+++ cygwin/web_pack.sh
@@ -1,9 +1,7 @@
-for p in httpd httpd-devel httpd-mod_php7 httpd-mod_ssl
-	do cyg_pack -p $p
-	done
-for p in php php-json php-iconv php-zip php-phar php-zlib php-tokenizer\
-	php-simplexml php-xmlwriter php-mbstring php-pgsql php-curl\
-	php-ctype php-gd php-intl php-fileinfo php-xmlrpc php-soap\
-	php-xmlreader php-opcache
-	do cyg_pack -p $p
-	done
+h=httpd,httpd-devel,httpd-mod_php7,httpd-mod_ssl
+cyg_pack -p $h
+p=php,php-json,php-iconv,php-zip,php-phar,php-zlib,php-tokenizer,\
+php-simplexml,php-xmlwriter,php-mbstring,php-pgsql,php-curl,\
+php-ctype,php-gd,php-intl,php-fileinfo,php-xmlrpc,php-soap,\
+php-xmlreader,php-opcache
+cyg_pack -p $p

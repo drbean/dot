7d119241626b1980915f01641501b4b4943a7b33
drbean <drbean@freeshell.org>
2020-09-28 16:52:55 +0800

    python3,should_be_python37?

--- chocolatey/choco_package.sh
+++ chocolatey/choco_package.sh
@@ -6,7 +6,7 @@ for p in subversion openssh znc screen
 		http://ftp.ntu.edu.tw/cygwin/ --packages $p
 	done
 p=weechat,lftp,vim,mutt,exim,lynx,weechat-perl,\
-	weechat-python,perl,gcc-g++,make,libcrypt-devel
+	weechat-python,perl,gcc-g++,make,libcrypt-devel,python3
 /cygdrive/c/tools/cygwin/cygwinsetup.exe --quiet-mode --no-desktop \
 	--no-startmenu --root c:/tools/cygwin --local-package-dir \
 	c:/tools/cygwin/packages --site http://ftp.ntu.edu.tw/cygwin/ \

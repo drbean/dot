62393d13db62c43145e5b33e143af5df51516090
drbean <drbean@freeshell.org>
2020-09-29 15:41:49 +0800

    cpan::shell_notest_Grades_install.should_remove_dependency!

--- cygwin/moopl_setup.sh
+++ cygwin/moopl_setup.sh
@@ -1,6 +1,7 @@
 svn up ~/ttb/moodle
 cd ~/ttb/moodle
 perl -MCPAN -e "CPAN->shell->notest('install', 'App::Cmd')"
+perl -MCPAN -e "CPAN->shell->notest('install', 'Grades')"
 cyg_pack -p perl-ExtUtils-MakeMaker
 perl Makefile.PL INST_SCRIPT=script
 make

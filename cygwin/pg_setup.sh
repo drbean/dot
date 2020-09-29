bf3a2c2d464436a91c3a3e86dbb94d74e4c2425c
drbean <drbean@freeshell.org>
2020-09-29 10:22:04 +0800

    cyg_pack_postgres_packages.s/for_loop/comma-separated_packages

--- cygwin/pg_setup.sh
+++ cygwin/pg_setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-for p in postgresql libpq5 libpq-devel libpgtypes3 postgresql-client postgresql-devel ; do cyg_pack -p $p ; done
+p=postgresql,libpq5,libpq-devel,libpgtypes3,postgresql-client,postgresql-devel
+cyg_pack -p $p 
 sed -iBAK -r -e "s/^INITDB_ARGS=.*$/INITDB_ARGS=\"-E UTF8 --locale=en_US.UTF-8 -D \$PGDATA\"/" /etc/rc.d/init.d/postgresql
 cygserver-config
 cygrunsrv --start cygserver

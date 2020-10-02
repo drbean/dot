c296f352625a03e707eecabbe7691c6e6de2478e
drbean <drbean@freeshell.org>
2020-10-02 16:41:17 +0800

    moosh_commands_to_populate_toeic

---
+++ cygwin/moodle_populate.pl
@@ -0,0 +1,2 @@
+Moosh -n course-create -f 'Teach Yourself TOEIC Speaking' -d 'TOEIC Speaking prep: quizzes, slides, practice tests' -F topics -n 15 toeic
+for s in general read pic question information solution opinion ; do Moosh -n gradecategory-create

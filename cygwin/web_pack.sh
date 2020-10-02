8969ee6361284e17ece999f010489b70e5a2baf2
drbean <drbean@freeshell.org>
2020-10-02 16:05:56 +0800

    php-posix

--- cygwin/web_pack.sh
+++ cygwin/web_pack.sh
@@ -3,5 +3,5 @@ cyg_pack -p $h
 p=php,php-json,php-iconv,php-zip,php-phar,php-zlib,php-tokenizer,\
 php-simplexml,php-xmlwriter,php-mbstring,php-pgsql,php-curl,\
 php-ctype,php-gd,php-intl,php-fileinfo,php-xmlrpc,php-soap,\
-php-xmlreader,php-opcache
+php-xmlreader,php-opcache,php-posix
 cyg_pack -p $p

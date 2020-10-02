fe992275745331ca9e103d007ca7431aaed9ed4f
drbean <drbean@freeshell.org>
2020-10-02 14:42:03 +0800

    cron

--- chocolatey/choco_package.sh
+++ chocolatey/choco_package.sh
@@ -7,7 +7,7 @@ for p in subversion openssh znc screen
 	done
 p=weechat,lftp,vim,mutt,exim,lynx,weechat-perl,\
 weechat-python,perl,gcc-g++,make,libcrypt-devel,libnsl-devel,\
-perl-YAML,perl-List-MoreUtils
+perl-YAML,perl-List-MoreUtils,cron
 /cygdrive/c/tools/cygwin/cygwinsetup.exe --quiet-mode --no-desktop \
 	--no-startmenu --root c:/tools/cygwin --local-package-dir \
 	c:/tools/cygwin/packages --site http://ftp.ntu.edu.tw/cygwin/ \

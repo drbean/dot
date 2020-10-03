adb373e87bf7e3223e19585b54b6edf68d2b0fde
drbean <drbean@freeshell.org>
2020-10-03 17:14:27 +0800

    yaml4moodle

--- cygwin/repo_checkout.sh
+++ cygwin/repo_checkout.sh
@@ -1,5 +1,5 @@
 cd
-for p in dot edit ttb curriculum; do svn co http://github.com/drbean/$p/trunk $p --depth=empty ; done
+for p in dot edit ttb curriculum yaml4moodle; do svn co http://github.com/drbean/$p/trunk $p --depth=empty ; done
 for u in bash screen vim chocolatey cygwin browser postgres ; do svn up dot/$u ; done
 # svn up edit/joyce
 for ext in rc _profile ; do cp dot/bash/bash$ext ~/.bash$ext ; done
@@ -15,3 +15,5 @@ znc
 for u in postgres ; do svn up ~/dot/$u ; done
 git clone --depth 2 --no-single-branch http://github.com/drbean/moosh ~/moosh
 mkdir --parent /srv/www/cgi-bin && git clone --depth 2 --no-single-branch http://github.com/drbean/moodle /srv/www/cgi-bin/moodle && mkdir /var/lib/moodle/ && chmod 777 /var/lib/moodle
+cd 
+svn up yaml4moodle

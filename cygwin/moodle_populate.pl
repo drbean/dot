94abbc508296f164d07c314621da0ed3c3a70323
drbean <drbean@freeshell.org>
2020-10-04 21:35:26 +0800

    how_avoid_hard-coding_qcat,top_cat,grade_cat,context?

--- cygwin/moodle_populate.pl
+++ cygwin/moodle_populate.pl
@@ -1,9 +1,22 @@
-# Moosh -n course-create -f 'Teach Yourself TOEIC Speaking' -d 'TOEIC Speaking prep: quizzes, slides, practice tests' -F topics -n 15 toeic_1
+$course = Moosh -n course-create -f 'Teach Yourself TOEIC Speaking' -d 'TOEIC Speaking prep: quizzes, slides, practice tests' -F topics -n 15 toeic_2
+# course=5
+top_cat=25
 task=(general read pic question information solution opinion)
-# ex_cat=$(Moosh -n gradecategory-create -n exercises 1 3)
-ex_cat=6
-for c in {0..6} ; do Moosh -n gradecategory-create -n ${task[$c]} $ex_cat 3 ; done
-# test_cat=$(Moosh -n gradecategory-create -n tests 1 3)
-test_cat=7
-for e in {0..4} ; do Moosh -n gradecategory-create -n "practice_test_$e" $test_cat 3 ; done
-Moosh -n questioncategory-create --reuse -p 4 -c 25 -d 'How to read a text' read
+ex_cat=$(Moosh -n gradecategory-create -n exercises $top_cat $course)
+# ex_cat=6
+for c in {0..6}
+	do Moosh -n gradecategory-create -n ${task[$c]} $ex_cat $course
+done
+test_cat=$(Moosh -n gradecategory-create -n tests $top_cat $course)
+# test_cat=7
+for e in {0..4}
+	do Moosh -n gradecategory-create -n "practice_test_$e" $test_cat $course
+done
+context=35
+parent=12
+q_cat=Moosh -n questioncategory-create --reuse -p $parent -c $context -d 'How to read a text' read
+# q_cat=17
+grade_cat=28
+sect='general'
+course_name='toeic_2'
+moopl section_populate -s $sect -q $q_cat -g $grade_cat -c $course -n $course_name

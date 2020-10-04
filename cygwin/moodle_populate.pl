d90e2d2721cf6c6232b373a88261221463b6c0c5
drbean <drbean@freeshell.org>
2020-10-04 17:02:43 +0800

    comment_out:course-create,gradecategory-create:exercises,tests.course_id.s/2/3/.need_to_check!

--- cygwin/moodle_populate.pl
+++ cygwin/moodle_populate.pl
@@ -1,7 +1,9 @@
-Moosh -n course-create -f 'Teach Yourself TOEIC Speaking' -d 'TOEIC Speaking prep: quizzes, slides, practice tests' -F topics -n 15 toeic
+# Moosh -n course-create -f 'Teach Yourself TOEIC Speaking' -d 'TOEIC Speaking prep: quizzes, slides, practice tests' -F topics -n 15 toeic_1
 task=(general read pic question information solution opinion)
-ex_cat=$(Moosh -n gradecategory-create -n exercises 1 2)
-for c in {0..6} ; do Moosh -n gradecategory-create -n ${task[$c]} $ex_cat 2 ; done
-test_cat=$(Moosh -n gradecategory-create -n tests 1 2)
-for e in {0..4} ; do Moosh -n gradecategory-create -n "practice_test_$e" $test_cat 2 ; done
+# ex_cat=$(Moosh -n gradecategory-create -n exercises 1 3)
+ex_cat=6
+for c in {0..6} ; do Moosh -n gradecategory-create -n ${task[$c]} $ex_cat 3 ; done
+# test_cat=$(Moosh -n gradecategory-create -n tests 1 3)
+test_cat=7
+for e in {0..4} ; do Moosh -n gradecategory-create -n "practice_test_$e" $test_cat 3 ; done
 Moosh -n questioncategory-create --reuse -p 4 -c 25 -d 'How to read a text' read

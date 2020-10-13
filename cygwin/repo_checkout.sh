9ea10bbc377149223f6f02c8a36f2c25d407cca2
drbean <drbean@freeshell.org>
2020-10-13 20:28:41 +0800

    update_only_used_dot,curriculum,ttb_files,with_parents

--- cygwin/repo_checkout.sh
+++ cygwin/repo_checkout.sh
@@ -1,19 +1,33 @@
 cd
 for p in dot edit ttb curriculum ; do svn co http://github.com/drbean/$p/trunk $p --depth=empty ; done
-for u in bash screen vim chocolatey cygwin browser postgres ; do svn up dot/$u ; done
-# svn up edit/joyce
-for ext in rc _profile ; do cp dot/bash/bash$ext ~/.bash$ext ; done
-cd ~/dot && svn up alternatives.sh && . alternatives.sh && cd -
+
+cd ~/dot
+
+for u in chocolatey cygwin ; do svn up $u ; done
+svn up alternatives.sh && . alternatives.sh
+svn up bash/bashrc bash/bash_profile --parents
+svn up screen/{screen,course,moodle,cygwin}.rc --parents
+svn up vim/vimrc.cygwin --parents
+svn up postgres/populate.sql --parents
+svn up browser/lynx{-site.cfg,.lss} --parents
+
+for ext in rc _profile ; do cp ~/dot/bash/bash$ext ~/.bash$ext ; done
 cp ~/dot/vim/vimrc.cygwin ~/.vimrc
 cp ~/dot/screen/screen.rc ~/.screenrc
 cp ~/dot/browser/lynx{-site.cfg,.lss} /etc/
 for i in znc weechat ; do svn co http://github.com/drbean/dot/trunk/irc/$i ~/.$i ; done
-for p in pages speaking online; do svn up curriculum/$p ; done
+
+cd ~/curriculum
+
+for p in online; do svn up curriculum/$p ; done
+svn up ~/curriculum/pages/homepage/markdown --parents
+svn up ~/curriculum/speaking/test/{online,cards.yaml} --parents
+for p in pandoc-templates ; do svn co http://github.com/drbean/$p/trunk ~/curriculum/pages/$p --depth=empty && svn up ~/curriculum/pages/$p/homepagePost.html5 ; done
+
+cd 
+
 svn co http://github.com/drbean/yaml4moodle/trunk ttb/yaml4moodle --depth=empty
 for p in moodle yaml4moodle ; do svn up ttb/$p/{lib,script,Makefile.PL} ; done
-for p in pandoc-templates ; do svn co http://github.com/drbean/$p/trunk ~/curriculum/pages/$p ; done
 znc
-for u in postgres ; do svn up ~/dot/$u ; done
 git clone --depth 2 --no-single-branch http://github.com/drbean/moosh ~/moosh
 mkdir --parent /srv/www/cgi-bin && git clone --depth 2 --no-single-branch http://github.com/drbean/moodle /srv/www/cgi-bin/moodle && mkdir /var/lib/moodle/ && chmod 777 /var/lib/moodle
-cd 

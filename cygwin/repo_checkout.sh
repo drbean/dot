73a784b9c22d1d5d21b4cffc409f685c08cb9a84
drbean <drbean@freeshell.org>
2020-10-20 21:41:39 +0800

    coc-settings.json,npm_install_bash-language-server,cmd_mklink

--- cygwin/repo_checkout.sh
+++ cygwin/repo_checkout.sh
@@ -7,12 +7,13 @@ for u in chocolatey cygwin ; do svn up $u ; done
 svn up alternatives.sh && . alternatives.sh
 svn up bash/bashrc bash/bash_profile --parents
 svn up screen/{dot,screen,course,moodle,cygwin,utility,web,irc,pagestranscripts}.rc --parents
-svn up vim/vimrc.cygwin --parents
+svn up vim/{vimrc.cygwin,coc-settings.json} --parents
 svn up postgres/populate.sql --parents
 svn up browser/lynx{-site.cfg,.lss} --parents
 
 for ext in rc _profile ; do cp ~/dot/bash/bash$ext ~/.bash$ext ; done
 cp ~/dot/vim/vimrc.cygwin ~/.vimrc
+cp ~/dot/vim/coc-settings.json ~/.vim/ --parents
 cp ~/dot/screen/screen.rc ~/.screenrc
 cp ~/dot/browser/lynx{-site.cfg,.lss} /etc/
 for i in znc weechat ; do svn co http://github.com/drbean/dot/trunk/irc/$i ~/.$i ; done
@@ -36,7 +37,8 @@ mkdir --parent /srv/www/cgi-bin && git clone --depth 2 --no-single-branch http:/
 # curl -sL install-node.now.sh | sh
 mkdir -p ~/.vim/pack/git-plugins/start
 for v in delimitMate vim-surround ale ultisnips coc.nvim ; do git clone --depth 1 https://github.com/drbean/$v.git ~/.vim/pack/git-plugins/start/$v ; done
-echo '{"diagnostic.displayByAle": true}' > ~/.vim/coc-settings.json
 cd /cygdrive/c/Program\ Files/nodejs/ && npm install yarn && cd -
 ln -s /cygdrive/c/tools/cygwin/home /cygdrive/c/home # not working? try in pshell:
 # New-Item -Path C:\home -ItemType SymbolicLink -Value C:\tools\cygwin\home
+# cmd /c mklink /D c:\home c:\tools\cygwin\home
+cd /cygdrive/c/Users/AppData/Roaming/npm && npm install bash-language-server && cd -

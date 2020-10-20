cb0d28e50d25335921894850706c5357384ca6d9
drbean <drbean@freeshell.org>
2020-10-20 16:26:24 +0800

    s/->shell/::Shell/.duplicates_cpan_pack,moopl_setup.should_be_removed?

--- cygwin/pm_setup.sh
+++ cygwin/pm_setup.sh
@@ -1,8 +1,8 @@
 svn up ~/ttb/moodle
 cd ~/ttb/moodle
-perl -MCPAN -e "CPAN->shell->notest('install', 'App::Cmd')"
-perl -MCPAN -e "CPAN->shell->notest('install', 'Grades')"
-perl -MCPAN -e "CPAN->shell->notest('install', 'App::Ack')"
+perl -MCPAN -e "CPAN::Shell->notest('install', 'App::Cmd')"
+perl -MCPAN -e "CPAN::Shell->notest('install', 'Grades')"
+perl -MCPAN -e "CPAN::Shell->notest('install', 'App::Ack')"
 cyg_pack -p perl-ExtUtils-MakeMaker
 perl Makefile.PL INST_SCRIPT=script
 make

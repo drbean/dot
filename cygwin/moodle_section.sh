12ef1090f1749136a8f707dc0783828b0f57a8a1
drbean <drbean@freeshell.org>
2020-10-26 12:52:22 +0800

    section:solution

--- cygwin/moodle_section.sh
+++ cygwin/moodle_section.sh
@@ -4,9 +4,9 @@ course=2
 course_name='speaking/test'
 context=25 # 'toeic' context
 parent=4 # 4 | Default for toeic
-section=('read' 'pic' 'question' 'information')
-info=('How to read a text' 'Describing pictures' 'Answering questions' 'Understanding information')
-for i in 3 ; do
+section=('read' 'pic' 'question' 'information' 'solution')
+info=('How to read a text' 'Describing pictures' 'Answering questions' 'Understanding information' 'Solving problems')
+for i in 3 4 ; do
 q_cat=$(Moosh -n questioncategory-create --reuse -p $parent -c $context -d "${info[$i]}" ${section[$i]})
 	echo "${section[$i]} q_cat=$q_cat"
 	grade_cat=$(($i+4))

7655325dadbebc53aeb77ddeeb51a18715d1e860
drbean <drbean@freeshell.org>
2020-11-01 21:33:24 +0800

    texlive,fonts:libertine,noto

---
+++ cygwin/tex_pack.sh
@@ -0,0 +1,5 @@
+ps=(basic langcjk xetex latex latexextra fontsrecommended pictures)
+c='linux-libertine-fonts,noto-cjk-fonts,noto-cjk-fonts-common,texlive'
+for p in ${ps[*]} ; do c="$c,texlive-collection-$p"
+done
+cyg_pack -p $c

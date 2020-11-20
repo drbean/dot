a451a60e4952b9cce2575d8d468f1f6fb8199fae
drbean <drbean@freeshell.org>
2020-11-20 15:07:06 +0800

    dummy_quiz=1,toeic_category=8,question-import:dummy,jigsaw,0

--- cygwin/moodle_course.sh
+++ cygwin/moodle_course.sh
@@ -6,20 +6,17 @@ course_name='speaking/test'
 
 dummy=$(Moosh -n activity-add --section 0 --name dummy -o="--visible=0" quiz $course)
 echo "dummy=$dummy"
-dummy_quiz=$(($dummy+1))
+dummy_quiz=$(($dummy+0))
 # dummy_quiz=2
-dummy_category=8
+toeic_category=8
 
-#question=$(perl -MYAML4Moodle::Command::xml -e "YAML4Moodle::Command::xml::execute('',{
-#	c=>$course_name, t=>'general',
-#	s=>'handbook', q=>'jigsaw', f=>0})")
-#echo "question=$question"
-#file='/var/lib/moodle/repository/general/quiz_handbook_jigsaw_0.xml'
-#echo $question > $file
-#quiz=$(Moosh -n question-import $file $dummy_quiz $dummy_category)
+file='/var/lib/moodle/repository/general/quiz_dummy_jigsaw_0.xml'
+perl -MYAML4Moodle::Command::xml -e 'print YAML4Moodle::Command::xml::execute(
+	"", { c=>"speaking/test", t=>"general", s=>"dummy", q=>"jigsaw", f=>0 }
+	);' > $file
+question=$(Moosh -n question-import $file $dummy_quiz $toeic_category)
 #Moosh -n activity-delete $dummy_quiz
 
-# on creating dummy quiz in General Notifications??
 top_cat=1
 ex_cat=$(Moosh -n gradecategory-create -n exercises $top_cat $course)
 echo "ex_grade_cat=$ex_cat"

a5ba9bd4fe6c7640bc3491a33cf01c71e725a0ef
drbean <drbean@freeshell.org>
2020-11-20 11:31:02 +0800

    c:home=c:cygwin.home

--- chocolatey/setup_cygwin.ps1
+++ chocolatey/setup_cygwin.ps1
@@ -1,1 +1,2 @@
  c:\Users\owuser\Downloads\setup-x86_64.exe --quiet-mode --root c:/cygwin64 --local-package-dir c:/Users/owuser/Downloads --site http://ftp.ntu.edu.tw/cygwin/
+ cmd /c mklink /d c:\home c:\cygwin64\home

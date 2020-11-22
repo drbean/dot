9580f3d10d59080c3b89caef209ecad2b773bd1f
drbean <drbean@freeshell.org>
2020-11-22 17:21:55 +0800

    dummy_quiz,toeic_context,top_category,toeic_category,YAML4Moodle:out

--- cygwin/moodle_course.sh
+++ cygwin/moodle_course.sh
@@ -7,14 +7,17 @@ course_name='speaking/test'
 dummy=$(Moosh -n activity-add --section 0 --name dummy -o="--visible=0" quiz $course)
 echo "dummy=$dummy"
 dummy_quiz=$(($dummy+0))
-# dummy_quiz=2
-toeic_category=8
+# dummy_quiz=1
+toeic_context=16
+top_cat=$(Moosh -n questioncategory-create --parent 0 --context $toeic_context top)
+toeic_cat=$(Moosh -n questioncategory-create --parent $top_cat --context $toeic_context "toeic default")
+# toeic_cat=4
 
 file='/var/lib/moodle/repository/general/quiz_dummy_jigsaw_0.xml'
-perl -MYAML4Moodle::Command::xml -e 'print YAML4Moodle::Command::xml::execute(
+perl -MMoodle::Command::xml -e 'print Moodle::Command::xml::execute(
 	"", { c=>"speaking/test", t=>"general", s=>"dummy", q=>"jigsaw", f=>0 }
 	);' > $file
-question=$(Moosh -n question-import $file $dummy_quiz $toeic_category)
+question=$(Moosh -n question-import $file $dummy_quiz $toeic_cat)
 #Moosh -n activity-delete $dummy_quiz
 
 top_cat=1

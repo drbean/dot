2e4783ca3d57822c031eb509c2659ad312e3beec
drbean <drbean@freeshell.org>
2020-11-23 13:15:53 +0800

    question-import.s/toeic_cat/general_cat/,gradecategory.s/top_cat/total_grade/

--- cygwin/moodle_course.sh
+++ cygwin/moodle_course.sh
@@ -13,23 +13,24 @@ top_cat=$(Moosh -n questioncategory-create --parent 0 --context $toeic_context t
 toeic_cat=$(Moosh -n questioncategory-create --parent $top_cat --context $toeic_context "toeic default")
 general_cat=$(Moosh -n questioncategory-create --parent $toeic_cat --context $toeic_context "general")
 # toeic_cat=4
+# general_cat=
 
 file='/var/lib/moodle/repository/general/quiz_dummy_jigsaw_0.xml'
 perl -MMoodle::Command::xml -e 'print Moodle::Command::xml::execute(
 	"", { c=>"speaking/test", t=>"general", s=>"dummy", q=>"jigsaw", f=>0 }
 	);' > $file
-question=$(Moosh -n question-import $file $dummy_quiz $toeic_cat)
+question=$(Moosh -n question-import $file $dummy_quiz $general_cat)
 #Moosh -n activity-delete $dummy_quiz
 
-top_cat=1
-ex_cat=$(Moosh -n gradecategory-create -n exercises $top_cat $course)
+total_grade=1
+ex_cat=$(Moosh -n gradecategory-create -n exercises $total_grade $course)
 echo "ex_grade_cat=$ex_cat"
 # ex_cat=6
 task=(general read pic question information solution opinion)
 for c in {0..6}
 	do Moosh -n gradecategory-create -n ${task[$c]} $ex_cat $course
 done
-test_cat=$(Moosh -n gradecategory-create -n tests $top_cat $course)
+test_cat=$(Moosh -n gradecategory-create -n tests $total_grade $course)
 echo "test_grade_cat=$test_cat"
 # test_cat=7
 for e in {0..4}

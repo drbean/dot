bc0dd6bc36ea13975e833b2da1f526effb488dc0
drbean <drbean@freeshell.org>
2021-01-03 17:17:23 +0800

    powershell:must_quote_strings?

--- chocolatey/powerup_cygwin.ps1
+++ chocolatey/powerup_cygwin.ps1
@@ -1,4 +1,4 @@
-$first=subversion,openssh,znc,screen,git
+$first="subversion,openssh,znc,screen,git"
 
  c:\Users\owuser\Downloads\setup-x86_64.exe --quiet-mode --root c:/cygwin64 --local-package-dir c:/Users/owuser/Downloads --site http://ftp.ntu.edu.tw/cygwin/ --packages $first
  cmd /c mklink /d c:\home c:\cygwin64\home

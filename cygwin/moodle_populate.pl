81325ea3686d3ccb55200ea2768b17a7e478ffb7
drbean <drbean@freeshell.org>
2021-03-27 14:36:56 +0800

    from_moodle.rc:/var/lib/moodle/repository_creation

--- cygwin/moodle_populate.pl
+++ cygwin/moodle_populate.pl
@@ -1,5 +1,11 @@
 task=(general read pic question information solution opinion)
 
+# mkdir --parent /srv/www/cgi-bin/ /var/lib/moodle/
+
+for t in general read pic question information solution opinion
+	do mkdir /var/lib/moodle/repository/$t --parent
+done
+
 . ~/dot/cygwin/moodle_course.sh
 
 . ~/dot/cygwin/moodle_section.sh

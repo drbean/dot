756c8c013da4edc524204787025283b3cbfa5f74
drbean <drbean@freeshell.org>
2021-05-10 21:14:27 +0800

    comment_out:App::Cmd,Grades,App::Ack

--- cygwin/cpan_pack.sh
+++ cygwin/cpan_pack.sh
@@ -1,6 +1,6 @@
 cyg_pack gcc-g++,make,libcrypt-devel,libnsl-devel
 # perl -MCPAN -e "CPAN::Shell->notest('install', 'Module::Build::XSUtil')"
 # perl -MCPAN -e "CPAN::Shell->notest('install', 'Perl::LanguageServer')"
-perl -MCPAN -e "CPAN::Shell->notest('install', 'App::Cmd')"
-perl -MCPAN -e "CPAN::Shell->notest('install', 'Grades')"
-perl -MCPAN -e "CPAN::Shell->notest('install', 'App::Ack')"
+# perl -MCPAN -e "CPAN::Shell->notest('install', 'App::Cmd')"
+# perl -MCPAN -e "CPAN::Shell->notest('install', 'Grades')"
+# perl -MCPAN -e "CPAN::Shell->notest('install', 'App::Ack')"

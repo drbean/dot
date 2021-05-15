c5e9c51639494c47dd2eccc963e7ae69fbcb4ff8
drbean <drbean@freeshell.org>
2021-05-15 16:41:21 +0800

    perl-JSON-XS

--- cygwin/twitter_setup.sh
+++ cygwin/twitter_setup.sh
@@ -1,5 +1,5 @@
 svn up ~/emoeco
-cyg_pack perl-ExtUtils-MakeMaker,make,gcc-g++,libcrypt-devel,libnsl-devel,perl-Module-Build,perl-Module-Build-Tiny,perl-Net-SSLeay,openssl,libssl-devel,perl-JSON-MaybeXS,perl-Mozilla-CA,perl-Test-Warnings,perl-Test-Deep,perl-Params-Util,perl-Sub-Install,perl-Package-Stash,perl-Sub-Exporter-Progressive,perl-Eval-Closure,perl-Scalar-List-Utils,perl-File-pushd,perl-Variable-Magic,perl-Data-Dump,perl-Class-Tiny,perl-File-HomeDir,perl-Params-Validate,perl-Path-Tiny,perl-Class-Inspector,perl-IO-String,perl-File-Listing,perl-Net-HTTP,perl-CGI,perl-File-Slurper,perl-Font-AFM,perl-Types-Serialiser
+cyg_pack perl-ExtUtils-MakeMaker,make,gcc-g++,libcrypt-devel,libnsl-devel,perl-Module-Build,perl-Module-Build-Tiny,perl-Net-SSLeay,openssl,libssl-devel,perl-JSON-MaybeXS,perl-Mozilla-CA,perl-Test-Warnings,perl-Test-Deep,perl-Params-Util,perl-Sub-Install,perl-Package-Stash,perl-Sub-Exporter-Progressive,perl-Eval-Closure,perl-Scalar-List-Utils,perl-File-pushd,perl-Variable-Magic,perl-Data-Dump,perl-Class-Tiny,perl-File-HomeDir,perl-Params-Validate,perl-Path-Tiny,perl-Class-Inspector,perl-IO-String,perl-File-Listing,perl-Net-HTTP,perl-CGI,perl-File-Slurper,perl-Font-AFM,perl-Types-Serialiser,perl-JSON-XS
 #for m in AnyEvent::Twitter::Stream Twitter::API Devel::REPL
 for m in AnyEvent::HTTP
 	do cd ~/emoeco

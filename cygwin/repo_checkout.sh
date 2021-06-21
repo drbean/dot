dff0759204661812f684e7f7e72c56df9c9de966
drbean <drbean@freeshell.org>
2021-06-21 11:59:29 +0800

    svn_up_edit_in_edit_screen_file

--- cygwin/repo_checkout.sh
+++ cygwin/repo_checkout.sh
@@ -28,7 +28,7 @@ for p in pandoc-templates ; do svn co http://github.com/drbean/$p/trunk ~/curric
 
 cd 
 
-for p in email jeffreykuo; do svn up edit/$p ; done
+# for p in email jeffreykuo; do svn up edit/$p ; done
 # for i in dapper-invoice; do svn co http://github.com/drbean/$i/trunk ~/edit/invoice/$i; done
 for p in moodle ; do svn up ~/ttb/$p/{lib,Makefile.PL} --parents; done
 znc

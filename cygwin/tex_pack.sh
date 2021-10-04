6c9e825cb6d15ffd2f4c90cb5cae752446a5c94c
drbean <drbean@freeshell.org>
2021-10-04 15:00:09 +0800

    latexrecommended:fontspec,langchinese:ctexhook

--- cygwin/tex_pack.sh
+++ cygwin/tex_pack.sh
@@ -1,4 +1,4 @@
-ps=(basic langcjk xetex latex latexextra fontsrecommended pictures)
+ps=(basic langcjk xetex latex latexrecommended latexextra fontsrecommended pictures langchinese)
 c='linux-libertine-fonts,noto-cjk-fonts,noto-cjk-fonts-common,texlive'
 for p in ${ps[*]} ; do c="$c,texlive-collection-$p"
 done

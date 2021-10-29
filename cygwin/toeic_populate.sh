212aff2f247afc9147c34eda320c8fb2dc10d4c3
drbean <drbean@freeshell.org>
2021-10-29 15:14:24 +0800

    s/moodle/toeic/

--- cygwin/toeic_populate.sh
+++ cygwin/toeic_populate.sh
@@ -6,6 +6,6 @@ for t in general read pic question information solution opinion
 	do mkdir /var/lib/moodle/repository/$t --parent
 done
 
-. ~/dot/cygwin/moodle_course.sh
+. ~/dot/cygwin/toeic_course.sh
 
-. ~/dot/cygwin/moodle_section.sh
+. ~/dot/cygwin/toeic_section.sh

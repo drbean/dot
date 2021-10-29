67b232318e3d0f1c30ec722f8185034512122967
drbean <drbean@freeshell.org>
2021-10-29 15:10:56 +0800

    s/toeic/toefl/,retaining_id_numbers!

--- cygwin/toefl_section.sh
+++ cygwin/toefl_section.sh
@@ -1,12 +1,12 @@
 # on creating question (editing quiz) for dummy quiz in General Notifications??
 # after creating file system repos
 course=2
-course_name='speaking/test'
-context=16 # 'toeic' context
+course_name='test/toefl'
+context=16 # 'toefl' context
 parent=4 # 4 | Default for toeic
-section=('general' 'read' 'pic' 'question' 'information' 'solution' 'opinion' 'test0' 'test1')
+section=('general' 'reading' 'listening' 'speaking' 'writing' 'test0' 'test1')
 
-for i in {0..8} ; do
+for i in {0..6} ; do
 	q_cat=${section[$i]}
 	q_cat_id=$(Moosh -n questioncategory-create --reuse -p $parent -c $context $q_cat)
 	echo "${section[$i]} q_cat=$q_cat"

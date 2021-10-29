7bf6aa890936d9d80d54ef972bb5cca36bb7dcc5
drbean <drbean@freeshell.org>
2021-10-29 15:01:56 +0800

    s/toeic/toefl/,retaining_id_numbers!

--- cygwin/toefl_course.sh
+++ cygwin/toefl_course.sh
@@ -1,24 +1,24 @@
-course=$(Moosh -n course-create -f 'Teach Yourself TOEIC Speaking' -d 'TOEIC Speaking prep: quizzes, slides, practice tests' -F topics -n 8 toeic)
+course=$(Moosh -n course-create -f 'Teach Yourself TOEFL' -d 'TOEFL prep: quizzes, slides, practice tests' -F topics -n 8 toefl)
 echo "course=$course"
 
 course=2
-course_name='speaking/test'
+course_name='test/toefl'
 
 dummy=$(Moosh -n activity-add --section 0 --name dummy -o="--visible=0" quiz $course)
 echo "dummy=$dummy"
 dummy_quiz=$(($dummy+0))
 # dummy_quiz=1
-toeic_context=3
-# top_cat=$(Moosh -n questioncategory-create --parent '""' --context $toeic_context top)
+toefl_context=3
+# top_cat=$(Moosh -n questioncategory-create --parent '""' --context $toefl_context top)
 top_cat=6
-toeic_cat=$(Moosh -n questioncategory-create --parent $top_cat --context $toeic_context "toeic default")
-general_cat=$(Moosh -n questioncategory-create --parent $toeic_cat --context $toeic_context "general")
-# toeic_cat=4
+toefl_cat=$(Moosh -n questioncategory-create --parent $top_cat --context $toefl_context "toefl default")
+general_cat=$(Moosh -n questioncategory-create --parent $toefl_cat --context $toefl_context "general")
+# toefl_cat=4
 # general_cat=
 
 file='/var/lib/moodle/repository/general/quiz_dummy_jigsaw_0.xml'
 perl -MMoodle::Command::xml -e 'print Moodle::Command::xml::execute(
-	"", { c=>"speaking/test", t=>"general", s=>"dummy", q=>"jigsaw", f=>0 }
+	"", { c=>"test/toefl", t=>"general", s=>"dummy", q=>"jigsaw", f=>0 }
 	);' > $file
 question=$(Moosh -n question-import $file $dummy_quiz $general_cat)
 #Moosh -n activity-delete $dummy_quiz
@@ -27,8 +27,8 @@ total_grade=1
 ex_cat=$(Moosh -n gradecategory-create -n exercises $total_grade $course)
 echo "ex_grade_cat=$ex_cat"
 # ex_cat=6
-task=(general read pic question information solution opinion)
-for c in {0..6}
+task=(general reading listening speaking writing)
+for c in {0..4}
 	do Moosh -n gradecategory-create -n ${task[$c]} $ex_cat $course
 done
 test_cat=$(Moosh -n gradecategory-create -n tests $total_grade $course)

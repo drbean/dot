7a2c3b6c453890673108516a091b6a9d25a25a40
drbean <drbean@freeshell.org>
2022-01-18 19:39:32 +0800

    s/owuser/$USER/

--- chocolatey/cygwin_package.sh
+++ chocolatey/cygwin_package.sh
@@ -11,12 +11,12 @@ if [[ -x /cygdrive/c/tools/cygwin/cygwinsetup.exe ]]
 			c:/tools/cygwin/packages --site \
 			http://ftp.ntu.edu.tw/cygwin/ --packages $p
 		done
-elif [ -x /cygdrive/c/Users/owuser/Downloads/setup-x86_64.exe ]
+elif [ -x /cygdrive/c/Users/$USER/Downloads/setup-x86_64.exe ]
 	then for p in "$second"
-		do /cygdrive/c/Users/owuser/Downloads/setup-x86_64.exe \
+		do /cygdrive/c/Users/$USER/Downloads/setup-x86_64.exe \
 			--quiet-mode --no-desktop --no-startmenu \
 			--root c:/cygwin64 --local-package-dir \
-			c:/Users/owuser/Downloads --site \
+			c:/Users/$USER/Downloads --site \
 			http://ftp.ntu.edu.tw/cygwin/ --packages $p
 		done
 fi

dc593cb0e4f2144c368328a348171dc1a64ba43f
drbean <drbean@freeshell.org>
2022-02-25 17:37:08 +0800

    q_cat.parent=s/9/4/.but_better_set_here_than_online/default.yaml

--- cygwin/toefl_section.sh
+++ cygwin/toefl_section.sh
@@ -3,7 +3,7 @@
 course=2
 course_name='test/toefl'
 context=12 # 16? #'toefl' context
-parent=9 # 7 # 9? # 4 | Default for toefl
+parent=4 # 7 # 9? # 4 | Default for toefl
 # parent id = 1 according to questioncategory-create
 section=('general' 'reading' 'listening' 'speaking' 'writing' 'test0' 'test1')
 

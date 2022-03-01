888f64f9b35791648f3c744ce4f67933ca55a319
drbean <drbean@freeshell.org>
2022-03-01 11:51:09 +0800

    course,parent,context,grade_cat_value_changes_if_course_not_=2

--- cygwin/toefl_section.sh
+++ cygwin/toefl_section.sh
@@ -1,15 +1,15 @@
 # on creating question (editing quiz) for dummy quiz in General Notifications??
 # after creating file system repos
-course=2
+course=2 # 3? if following course
 course_name='test/toefl'
-context=12 # 16? #'toefl' context
-parent=4 # 7 # 9? # 4 | Default for toefl
+context=12 # 125? 16? #'toefl' context
+parent=4 # 37 # 7 # 9? # 4 # Default for toefl
 # parent id = 1 according to questioncategory-create
 section=('general' 'reading' 'listening' 'speaking' 'writing' 'test0' 'test1')
 
 for i in {0..4} ; do
 	q_cat=${section[$i]}
-	grade_cat=$(($i+4))
+	grade_cat=$(($i+4)) # 17?
 	sect=${section[$i]}
 	if [[ $(perl -MMoodle::Command::section_populate -e 0 2>&1) ]] ; then echo 'run ~/dot/cygwin/moopl_setup.sh' && exit; fi
 	perl -MMoodle::Command::section_populate -e "Moodle::Command::section_populate::execute('', { s=>\"$sect\", q=>\"$q_cat\", p=>$parent, x=>$context, g=>$grade_cat, c=>$course, n=>\"$course_name\" })"

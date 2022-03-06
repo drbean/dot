908239ae308f1daba9679d9fab89bd727846659d
drbean <drbean@freeshell.org>
2022-03-06 14:00:42 +0800

    user=owuser,HOME,and_invoke_literal_as_code

--- chocolatey/powerup_cygwin.ps1
+++ chocolatey/powerup_cygwin.ps1
@@ -1,4 +1,5 @@
 $first="subversion,openssh,znc,screen,git"
+$user="owuser"
 
- c:\Users\$USER\Downloads\setup-x86_64.exe --quiet-mode --root c:/cygwin64 --local-package-dir c:/Users/$USER/Downloads --site http://ftp.ntu.edu.tw/cygwin/ --packages $first
+& $HOME\Downloads\setup-x86_64.exe --quiet-mode --root c:/cygwin64 --local-package-dir c:/Users/$user/Downloads --site http://ftp.ntu.edu.tw/cygwin/ --packages $first
  cmd /c mklink /d c:\home c:\cygwin64\home

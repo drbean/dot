0cf0a87e84d790511de50b19a0807ada2d597684
drbean <drbean@freeshell.org>
2022-10-11 13:22:39 +0800

    alternate_cygwin_mirror:ftp.ntu.edu.tw/pub/cygwin

---
+++ chocolatey/powerup_ntu_cygwin.ps1
@@ -0,0 +1,5 @@
+$first="subversion,openssh,znc,screen,git"
+$user="owuser"
+
+& $HOME\Downloads\setup-x86_64.exe --quiet-mode --root c:/cygwin64 --local-package-dir c:/Users/$user/Downloads --site ftp://ftp.ntu.edu.tw/pub/cygwin/ --packages $first
+ cmd /c mklink /d c:\home c:\cygwin64\home

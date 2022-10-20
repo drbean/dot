214af9c7358c3042d387ca6a7afbd9c7b3b47963
drbean <drbean@freeshell.org>
2022-10-20 13:33:58 +0800

    if,then_loop_for_twaren,ntu_downloads

--- chocolatey/cygwin_package.sh
+++ chocolatey/cygwin_package.sh
@@ -12,21 +12,23 @@ if [[ -x /cygdrive/c/tools/cygwin/cygwinsetup.exe ]]
 			http://ftp.ntu.edu.tw/cygwin/ --packages $p
 		done
 elif [ -x /cygdrive/c/Users/$USER/Downloads/setup-x86_64.exe ]
-	then for p in "$second"
-		do /cygdrive/c/Users/$USER/Downloads/setup-x86_64.exe \
+	then for p in "$second" ; do
+if [[ -d /cygdrive/c/Users/$USER/Downloads/ftp%3a%2f%2fftp.twaren.net%2fUnix%2fsourceware.org%2fcygwin%2f ]]
+	then 
+		/cygdrive/c/Users/$USER/Downloads/setup-x86_64.exe \
 			--quiet-mode --no-desktop --no-startmenu \
 			--root c:/cygwin64 --local-package-dir \
 			c:/Users/$USER/Downloads --site \
 			ftp://ftp.twaren.net/Unix/sourceware.org/cygwin \
 			--packages $p
-		if ! [[ -d /home/owuser ]] ; then 
-			/cygdrive/c/Users/$USER/Downloads/setup-x86_64.exe \
-				--quiet-mode --no-desktop \
-				--no-startmenu \
-				--root c:/cygwin64 \
-				--local-package-dir \
-				c:/Users/$USER/Downloads --site \
-				http://ftp.ntu.edu.tw/cygwin/ --packages $p
+else
+		/cygdrive/c/Users/$USER/Downloads/setup-x86_64.exe \
+			--quiet-mode --no-desktop \
+			--no-startmenu \
+			--root c:/cygwin64 \
+			--local-package-dir \
+			c:/Users/$USER/Downloads --site \
+			http://ftp.ntu.edu.tw/cygwin/ --packages $p
 		fi
 		done
 elif [ -x /cygdrive/d/Users/$USER/Downloads/setup-x86_64.exe ]

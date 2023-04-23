1abb2f593b52034c4b19160cf5b33d7b3ec630c0
drbean <drbean@freeshell.org>
2023-04-23 21:33:44 +0900

    address_List=eval_echo.multi_AREA

--- bash/remove_email.sh
+++ bash/remove_email.sh
@@ -2,13 +2,15 @@
 
 # was function ploop () {
 
-AREA=${AREA%/}
+address_list=$(eval echo $AREA/*/*/address.txt)
 while true ; do read -p '> ' -a p
 bad=${p[0]}
 if [[ $bad != *@* ]] ; then
     echo "No '@' in bad arg: \"${#p[@]}: ${p[@]}\""  >&2
     continue 1 ; fi
-school=$(grep -l "^$bad" $AREA/*/*/address.txt)
+school=$(grep -l "^$bad" $address_list)
+if [[ $bad =~ ^([^@]*@postech\.ac\.kr)[A-Z]+$ ]]; then 
+    p[1]="${BASH_REMATCH[1]}"; fi
 if [[ $school == *ntupes* ]]; then 
     p[1]="${bad%ntupes.edu.tw}ntus.edu.tw"; fi
 if [[ ${#p[@]} -gt 2 ]]; then
@@ -16,17 +18,17 @@ if [[ ${#p[@]} -gt 2 ]]; then
 elif [[ ${#p[@]} -eq 2 ]]; then
     good=${p[1]}
     sed -i.BAK -e "s/^\($bad\)\(\s\|$\)/$good # \1/" $school
-    svn diff $AREA/*/*/address.txt
+    svn diff $address_list
     screen -p 1 -X stuff \
     "echo $good | /sdf/udd/d/drbean/job/send.sh^M"
     echo "Sending to '$good' via send.sh on sdf."
-    svn ci $AREA/*/*/address.txt \
+    svn ci $address_list \
        -m "$bad: Edited
 $good"
 elif [[ ${#p[@]} -eq 1 ]]; then
     sed -i.BAK -e "s/^\($bad\)\(\s\|$\)/# \1/" $school
-    svn diff $AREA/*/*/address.txt;
-    svn ci $AREA/*/*/address.txt -m "$bad: User unknown"
+    svn diff $address_list;
+    svn ci $address_list -m "$bad: User unknown"
 else
     echo "No args: \"${#p[@]}: ${p[@]}\""  >&2
 fi

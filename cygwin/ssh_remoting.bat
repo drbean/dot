ba478bb0d6c4d202d70e41fde5c00fc80624abd1
drbean <drbean@freeshell.org>
2023-08-18 15:28:52 +0900

    CYGWIN,HOME,Azy4476,bashrc

--- cygwin/ssh_remoting.bat
+++ cygwin/ssh_remoting.bat
@@ -1,8 +1,6 @@
-ME%
 @ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%spawn.ps1
-C:\cygwin64\bin\bash -l c:\home\Azy4476\edit\trunk\email\open_faculty.sh %QUTE_FIFO%
-@REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'" >> %QUTE_FIFO%
-@REM echo open -t https://www.dict.cc >> %QUTE_FIFO%
-@REM echo open -t https://www.dict.cc
+SET CYGWIN=c:\cygwin64
+SET HOME=%CYGWIN%\home\Azy4476
+cd %HOME%
+%CYGWIN%\bin\bash --login --rcfile %HOME%\.bashrc -i
+REM pause

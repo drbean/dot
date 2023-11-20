5485bc887ab060639d0a83df7270297c762d45a9
drbean <drbean@freeshell.org>
2023-11-20 09:12:28 +0900

    Pr=hanil

--- bash/email.sh
+++ bash/email.sh
@@ -420,7 +420,7 @@ function rewrite_url () {
     trurl --url $old_url --set host=$host --set path=$path
 }
 
-alias Pr="premail -l kr -a west -c cenlapukto -s jnue -u http://www.jnue.ac.kr"
+alias Pr="premail -l kr -a west -c cenlapukto -s hanil -u http://www.hanil.ac.kr"
 # assemble an address list for a school
 function premail () {
     OPTIND=1

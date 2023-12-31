8efd44596bba80ab5db66e3adea51f0a05363943
drbean <drbean@freeshell.org>
2023-12-31 16:47:48 +0900

    Pr=hoseo

--- bash/email.sh
+++ bash/email.sh
@@ -477,7 +477,7 @@ function permute_url () {
     trurl --url $old_url --set host=$host --set path=$path
 }
 
-alias Pr="premail -l kr -a west -c chwungchengnamto -s koreatech -u http://www.koreatech.ac.kr"
+alias Pr="premail -l kr -a west -c chwungchengnamto -s hoseo -u http://www.hoseo.ac.kr"
 # assemble an address list for a school
 function premail () {
     OPTIND=1

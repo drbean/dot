2a7dd8845a73c7378b5265eed79deeda7f005c9e
Dr Bean <drbean@freeshell.org>
2024-06-02 18:20:18 +0900

    Pr=semyung

--- bash/email.sh
+++ bash/email.sh
@@ -526,7 +526,7 @@ function permute_url () {
     trurl --url $old_url --set host=$host --set path=$path
 }
 
-alias Pr="premail -l kr -a west -c chwungchengpukto -s chungbuk -u http://www.chungbuk.ac.kr"
+alias Pr="premail -l kr -a west -c chwungchengpukto -s semyung -u http://www.semyung.ac.kr"
 # assemble an address list for a school
 function premail () {
     OPTIND=1

0527e6b8b20adecddb2377c7a1719bff0f968cf9
Dr Bean <drbean@freeshell.org>
2024-08-23 09:20:02 +0900

    alternative.index.0 = w3m

--- alternatives.sh
+++ alternatives.sh
@@ -1,5 +1,7 @@
 # v alternative
 /usr/sbin/alternatives --install /usr/bin/v v /usr/bin/vim 50 --slave /usr/share/man/man1/v.1.gz v-man /usr/share/man/man1/vim.1.gz
 # links alternative
-for alternative in w3m lynx links ; do /usr/sbin/alternatives --install /usr/bin/lynk lynk /usr/bin/$alternative 50 --slave /usr/share/man/man1/lynk.1.gz lynk-man /usr/share/man/man1/$alternative.1.gz ; done
+alternative=(w3m lynx links)
+index=( ${!alternative[*]} )
+for i in ${index[*]} ; do /usr/sbin/alternatives --install /usr/bin/lynk lynk /usr/bin/${alternative[$i]} $((50+i)) --slave /usr/share/man/man1/lynk.1.gz lynk-man /usr/share/man/man1/${alternative[$i]}.1.gz ; done
 

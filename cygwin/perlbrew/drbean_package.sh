90b114a34fdf50003f1879955d9c54443b623f4b
Dr Bean <drbean@freeshell.org>
2025-03-03 23:08:25 +0900

    quiet-mode, no-desktop, no-startmenu *appears* to invalidate no-verify

--- cygwin/perlbrew/drbean_package.sh
+++ cygwin/perlbrew/drbean_package.sh
@@ -4,7 +4,7 @@ applist=perl-App-perlbrew
 
 if [ -x /cygdrive/c/Users/$USER/Downloads/setup-x86_64.exe ] ; then
 	/cygdrive/c/Users/$USER/Downloads/setup-x86_64.exe \
-		-X --quiet-mode --no-desktop --no-startmenu \
+		--no-verify --quiet-mode --no-desktop --no-startmenu \
 		--root c:/cygwin64 --local-package-dir \
 		c:/Users/$USER/Desktop --site \
 		http://drbean.sdf.org/cygwin \

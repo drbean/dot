fa64589684bf14e81423a3df58c640f74f0a5f50
Dr Bean <drbean@freeshell.org>
2026-09-05 13:47:41 +0900

    port screen stuff code to shell, but what is commented code ; plus homepage/markdown/get_story_out.sh pandoc, lftp, svn ci wrapper

--- screen/stowits/local.sh
+++ screen/stowits/local.sh
@@ -1,30 +1,62 @@
 # local
 
-screen 3
-stuff "cd ~/stow/^M"
-stuff "ls -al^M"
-stuff "declare -a cmd=( \"active_tooter\" \"blog.sh\" \"common.sh\" \"easyblogger\" \"masto_master_messter\" \"post.sh\" \"toot\" \"trurl\" \"webcaster.sh\")^M"
-stuff "declare -A key_cmd=( [mastodon]=\"{active_tooter,masto_master_messter,webcaster.sh}\" [posse]=\"{blog.sh,common.sh,pub}\" [.]=\"post.sh\" [posse/easyblogger/.venv39/bin]=\"easyblogger\" [../../dot/browser/toot/.venv.39/bin]=\"toot\" [../../edit/email/trurl]=\"trurl.exe\")^M"
-stuff "for dir in \${!key_cmd[*]} ; do for app in \$(eval echo \${key_cmd[\$dir]})  ; do echo \$dir: \$app ; diff -u ~/curriculum/pages/\$dir/\$app ~/stow/local/bin/\$app ; done ; done^M"
-stuff "# for dir in \${!key_cmd[@]} ; do for app in \${!key_cmd[\$dir]} ; do diff -u ~/curriculum/pages/\$dir/\$app ~/stow/local/bin/\$app ; done ; done^M"
-stuff "# for app in \${cmd[@]} ; do diff -u  ~/stow/local/bin/\$app /usr/local/bin/\$app ; done^M"
-stuff "# for app in \${cmd[@]} ; do rm ~/stow/local/bin/\$app ; done^M"
-stuff "# for app in \${cmd[@]} ; do ln ~/curriculum/pages/homepage/markdown/\$app ~/stow/local/bin/\$app ; done^M"
-stuff "# for dir in \${!key_cmd[@]} ; do"
-stuff "    for appl in \${key_cmd[\$dir]} ; do"
-stuff "	    echo appl: \${appl[@]} ; for app in \$(eval echo \$appl) ; do"
-stuff "            echo app: \$app ;"
-stuff "            diff -u ~/curriculum/pages/\$dir/\$app ~/stow/local/bin/\$app ;"
-stuff "        done ;"
-stuff "    done ;"
-stuff " done^M"
-stuff "for dir in \${!key_cmd[@]} ; do 
-    for app in \${!key_cmd[\$dir]} ; do
-        ln ~/curriculum/local/\$dir/\$app ~/stow/local/bin/\$app
-    done ; done^M"
-stuff "# stow -n --verbose=2 -t /usr/local -d ~drbean/stow -R local^M"
+cd ~/stow/
+echo pwd: $(pwd)
 
-# root Azy4476
+echo ls -al
+ls -al
+echo
 
-stuff "ssh root^M"
--
+declare -a cmd=( "active_tooter" "blog.sh" "common.sh" "easyblogger" "get_story_out.sh" "masto_master_messter" "post.sh" "pub" "toot" "trurl" "webcaster.sh")
+echo /usr/local/bin command="( ${cmd[@]} )"
+
+declare -A key_cmd=(
+    [mastodon]="{active_tooter,masto_master_messter,webcaster.sh}"
+    [posse]="{blog.sh,common.sh,pub}" [.]="post.sh"
+    [homepage/markdown]="get_story_out.sh"
+    [posse/easyblogger/.venv39/bin]="easyblogger"
+    [../../dot/browser/toot/.venv.39/bin]="toot"
+    [../../edit/email/trurl]="trurl.exe"
+)
+echo /usr/local/bin command package directories="( ${!key_cmd[@]} )"
+echo
+
+echo "Checking pages, etc, ~/stow diffs"
+for dir in ${!key_cmd[*]} ; do
+    for app in $(eval echo ${key_cmd[$dir]}) ; do
+        echo $dir: $app ; diff -u ~/curriculum/pages/$dir/$app ~/stow/local/bin/$app
+    done
+done
+echo
+
+#for dir in ${!key_cmd[@]} ; do for app in ${!key_cmd[$dir]} ; do diff -u ~/curriculum/pages/$dir/$app ~/stow/local/bin/$app ; done ; done
+#for app in ${cmd[@]} ; do diff -u  ~/stow/local/bin/$app /usr/local/bin/$app ; done
+
+echo "Removing stow dir links"
+for app in ${cmd[@]} ; do rm ~/stow/local/bin/$app ; done
+echo
+
+#for app in ${cmd[@]} ; do ln ~/curriculum/pages/homepage/markdown/$app ~/stow/local/bin/$app ; done
+#for dir in \${!key_cmd[@]} ; do
+#    for appl in \${key_cmd[\$dir]} ; do
+#        echo appl: \${appl[@]} ; for app in \$(eval echo \$appl) ; do
+#            echo app: \$app
+#            diff -u ~/curriculum/pages/\$dir/\$app ~/stow/local/bin/\$app
+#        done
+#    done
+#done
+
+echo "Linking pages commands <- ~/stow/local/bin"
+for dir in ${!key_cmd[@]} ; do 
+    for app in $(eval echo ${key_cmd[$dir]}) ; do
+        ln ~/curriculum/pages/$dir/$app ~/stow/local/bin/$app
+    done ; done
+echo
+
+#stow -n --verbose=2 -t /usr/local -d ~drbean/stow -R local
+#
+## root Azy4476
+
+ssh root
+
+exec bash -l

1afa309282cf26f6c9d9fea96a408c4b39f3c7c9
Dr Bean <drbean@freeshell.org>
2026-09-14 22:47:53 +0900

    toot_instance

--- screen/stowits/local.sh
+++ screen/stowits/local.sh
@@ -7,11 +7,11 @@ echo ls -al
 ls -al
 echo
 
-declare -a cmd=( "active_tooter" "blog.sh" "common.sh" "easyblogger" "get_story_out.sh" "masto_master_messter" "post.sh" "pub" "toot" "trurl" "webcaster.sh")
+declare -a cmd=( "active_tooter" "blog.sh" "common.sh" "easyblogger" "get_story_out.sh" "masto_master_messter" "post.sh" "pub" "toot" "toot_instance" "trurl" "webcaster.sh")
 echo /usr/local/bin command="( ${cmd[@]} )"
 
 declare -A key_cmd=(
-    [mastodon]="{active_tooter,masto_master_messter,webcaster.sh}"
+    [mastodon]="{active_tooter,masto_master_messter,toot_instance,webcaster.sh}"
     [posse]="{blog.sh,common.sh,pub}" [.]="post.sh"
     [homepage/markdown]="get_story_out.sh"
     [posse/easyblogger/.venv39/bin]="easyblogger"

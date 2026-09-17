168b6c47b2afad2938075a3a21032ef7aa499f74
Dr Bean <drbean@freeshell.org>
2026-09-17 18:01:38 +0900

    nl (cat -n option exists) screen/screenings/premail.sh , screen/premails/*.sh

--- screen/screenings/premail.sh
+++ screen/screenings/premail.sh
@@ -1,6 +1,18 @@
-cd ~/dot/screen/premails
-pwd ; ls . -B ; echo
-cat *.{sh,rc}; echo
-echo ../premail.sh
-cat ../premail.sh
+#!/usr/bin/bash
+
+echo ~/dot/screen/screening/premail.sh:
+echo
+
+nl ~/dot/screen/screenings/premail.sh
+echo
+
+echo ~/dot/screen/premails:
+echo
+
+for f in ~/dot/screen/premails/*.{sh,rc} ; do
+	echo -e $f:
+	nl $f
+	echo
+done
+
 exec bash -l

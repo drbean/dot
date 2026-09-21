7f80b8f03d98644a101ef729bffe78fca5e59af3
Dr Bean <drbean@freeshell.org>
2026-09-21 22:15:44 +0900

    screen 0 → edit files, screen 1 → get addresses, screen 3 → direct operation

--- screen/premails/premail.sh
+++ screen/premails/premail.sh
@@ -1,9 +1,12 @@
 cd ~/edit/email
 
+# edit files
 screen v prof_page.html sourcer.sh
 
+# get addresses
 screen bash -lc "< prof_page.html sourcer | curler | addre -l /mailto/ -p mailto: | vipe >> \$A"
 
+# direct operation
 screen svn st -q
 
 exec bash -l

1d1517596f235a87e3d2ad76a9a166ca5d673d63
Dr Bean <drbean@freeshell.org>
2026-09-23 17:17:21 +0900

    short → long form screenS/stowit, screen/stowits

--- screen/screenSs/stowit.sh
+++ screen/screenSs/stowit.sh
@@ -1,1 +1,18 @@
-cd ~/dot/screen/stowits && pwd && echo && ls -B && exec bash -l
+#!/usr/bin/bash
+
+echo ~/dot/screen/screenS/stowit.sh:
+echo
+
+nl ~/dot/screen/screenSs/stowit.sh
+echo
+
+echo ~/dot/screen/stowits:
+echo
+
+for f in ~/dot/screen/stowits/*.{sh,rc} ; do
+	echo -e $f:
+	nl $f
+	echo
+done
+
+exec bash -l
